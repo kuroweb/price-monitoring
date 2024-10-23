8ee5639682d7c496062e475dd2a1d71837dfc1ae
kuroweb <kuromitsu0104@gmail.com>
2024-10-24 00:13:53 +0900

    docs: update

--- volumes/backend/config/initializers/cors.rb
+++ volumes/backend/config/initializers/cors.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # TODO: config gem で origins の中身を切り替えられるようにする
     origins "localhost", "192.168.0.217"
     resource "*",
              headers: :any,
