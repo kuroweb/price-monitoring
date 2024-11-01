ced2a6ff74631154f857675f386951b2f73721ec
kuroweb <kuromitsu0104@gmail.com>
2024-11-02 00:52:06 +0900

    feat: add api

--- volumes/backend/config/routes.rb
+++ volumes/backend/config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products, only: %i[index]
+      resources :products, only: %i[index destroy]
+      resources :categories, only: %i[index]
     end
   end
 
