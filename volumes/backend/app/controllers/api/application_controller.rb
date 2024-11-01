d22292498bb0e0f81711e66314bc88a05a951cb6
kuroweb <kuromitsu0104@gmail.com>
2024-11-02 01:00:44 +0900

    feat: エラーレスポンスの調整

--- volumes/backend/app/controllers/api/application_controller.rb
+++ volumes/backend/app/controllers/api/application_controller.rb
@@ -8,15 +8,15 @@ module Api
     def internal_server_error(exception)
       Bugsnag.notify(exception)
       Rails.logger.error("Internal Server Error. exception: #{exception.full_message}")
-      render json: { error: "Internal Server Error.", status: 503 }, status: :service_unavailable
+      render json: { code: 503, message: "Internal Server Error.", details: [] }, status: :service_unavailable
     end
 
     def conflict_error
-      render json: { error: "Conflict.", status: 409 }, status: :conflict
+      render json: { code: 409, message: "Conflict.", details: [] }, status: :conflict
     end
 
     def not_found_error
-      render json: { error: "Not Found.", status: 404 }, status: :not_found
+      render json: { code: 404, message: "Not Found.", details: [] }, status: :not_found
     end
   end
 end
