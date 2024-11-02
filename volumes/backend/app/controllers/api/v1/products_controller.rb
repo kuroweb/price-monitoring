afc27a516e4360c6c09d5bbac59fde5f53c3821b
kuroweb <kuromitsu0104@gmail.com>
2024-11-02 22:39:08 +0900

    feat: add create endpoint

--- volumes/backend/app/controllers/api/v1/products_controller.rb
+++ volumes/backend/app/controllers/api/v1/products_controller.rb
@@ -6,6 +6,14 @@ module Api
         render json: ProductsSerializer.new(products), status: :ok
       end
 
+      def create
+        product = ::Products::Create.call(params: create_product_params)
+        render json: ProductSerializer.new(product), status: :ok
+      rescue ActiveRecord::RecordInvalid => e
+        Rails.logger.error("Bad Request. exception: #{e.full_message}")
+        render json: { code: 400, message: "Bad Request.", details: [] }, status: :bad_request
+      end
+
       def destroy
         product = Product.find(params[:id])
 
@@ -22,22 +30,48 @@ module Api
         params.permit(product_attributes + external_attributes)
       end
 
+      def create_product_params
+        params.permit(
+          product_attributes,
+          yahoo_auction_crawl_setting: yahoo_auction_crawl_setting_attributes,
+          mercari_crawl_setting: mercari_crawl_setting_attributes,
+          janpara_crawl_setting: janpara_crawl_setting_attributes,
+          iosys_crawl_setting: iosys_crawl_setting_attributes,
+          pc_koubou_crawl_setting: pc_koubou_crawl_setting_attributes,
+          used_sofmap_crawl_setting: used_sofmap_crawl_setting_attributes
+        )
+      end
+
       def product_attributes
         %i[id name]
       end
 
-      def external_attributes
-        %i[sort order]
+      def yahoo_auction_crawl_setting_attributes
+        %i[keyword category_id min_price max_price enabled]
       end
 
-      def build_error_json(code, message, details)
-        {
-          error: {
-            code:,
-            message:,
-            details:
-          }
-        }
+      def mercari_crawl_setting_attributes
+        %i[keyword category_id min_price max_price enabled]
+      end
+
+      def janpara_crawl_setting_attributes
+        %i[keyword min_price max_price enabled]
+      end
+
+      def iosys_crawl_setting_attributes
+        %i[keyword min_price max_price enabled]
+      end
+
+      def pc_koubou_crawl_setting_attributes
+        %i[keyword min_price max_price enabled]
+      end
+
+      def used_sofmap_crawl_setting_attributes
+        %i[keyword min_price max_price enabled]
+      end
+
+      def external_attributes
+        %i[sort order]
       end
     end
   end
