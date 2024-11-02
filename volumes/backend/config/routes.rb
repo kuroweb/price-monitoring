73d0d3286e310a4c6dc05bcae3f30f3936a594d9
kuroweb <kuromitsu0104@gmail.com>
2024-11-02 22:39:08 +0900

    feat: add create endpoint

--- volumes/backend/config/routes.rb
+++ volumes/backend/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products, only: %i[index destroy]
+      resources :products, only: %i[index create destroy]
       resources :categories, only: %i[index]
     end
   end
