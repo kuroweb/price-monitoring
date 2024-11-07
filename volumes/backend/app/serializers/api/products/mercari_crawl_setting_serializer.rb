dd9b2f162601cd547b1f7941fd43b99b2d5b9415
kuroweb <kuromitsu0104@gmail.com>
2024-11-08 01:16:07 +0900

    refactor: JSONデータを返却するだけのシンプルなシリアライザに変更する

---
+++ volumes/backend/app/serializers/api/products/mercari_crawl_setting_serializer.rb
@@ -0,0 +1,32 @@
+module Api
+  module Products
+    class MercariCrawlSettingSerializer < BaseSerializer
+      def initialize(mercari_crawl_setting)
+        @mercari_crawl_setting = mercari_crawl_setting
+      end
+
+      private
+
+      def json
+        mercari_crawl_setting_json.merge(
+          {
+            mercari_crawl_setting_exclude_keywords: mercari_crawl_setting_exclude_keywords_json,
+            mercari_crawl_setting_required_keywords: mercari_crawl_setting_required_keywords_json
+          }
+        )
+      end
+
+      def mercari_crawl_setting_json
+        @mercari_crawl_setting.as_json
+      end
+
+      def mercari_crawl_setting_exclude_keywords_json
+        @mercari_crawl_setting.mercari_crawl_setting_exclude_keywords.map(&:as_json)
+      end
+
+      def mercari_crawl_setting_required_keywords_json
+        @mercari_crawl_setting.mercari_crawl_setting_required_keywords.map(&:as_json)
+      end
+    end
+  end
+end
