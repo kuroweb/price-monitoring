65ac905e89c5b120ab8ca95a008eafda6215487f
kuroweb <kuromitsu0104@gmail.com>
2024-11-26 06:08:02 +0900

    feat: exclude_keyword, required_keywordの更新パラメータを受け取れるようにする

--- volumes/backend/app/controllers/api/v1/products_controller.rb
+++ volumes/backend/app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class ProductsController < Api::ApplicationController
+    class ProductsController < Api::ApplicationController # rubocop:disable Metrics/ClassLength
       def index
         products = ProductFinder.new(params: find_product_params).execute
         render json: Api::Products::ProductListSerializer.new(products).as_json, status: :ok
@@ -11,12 +11,12 @@ module Api
       end
 
       def create
-        product = ::Products::Create.call(params: create_product_params)
+        product = ::Products::Create.call(params: upsert_product_params)
         render json: product.as_json, status: :ok
       end
 
       def update
-        ::Products::Update.call(product:, params: update_product_params)
+        ::Products::Update.call(product:, params: upsert_product_params)
         inspect
         render json: product.as_json, status: :ok
       end
@@ -39,28 +39,45 @@ module Api
         params.permit(product_attributes + external_attributes)
       end
 
-      def create_product_params
+      def upsert_product_params # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
         params.permit(
           product_attributes + [
-            yahoo_auction_crawl_setting: yahoo_auction_crawl_setting_attributes,
-            mercari_crawl_setting: mercari_crawl_setting_attributes,
-            janpara_crawl_setting: janpara_crawl_setting_attributes,
-            iosys_crawl_setting: iosys_crawl_setting_attributes,
-            pc_koubou_crawl_setting: pc_koubou_crawl_setting_attributes,
-            used_sofmap_crawl_setting: used_sofmap_crawl_setting_attributes
-          ] + [:category_id]
-        )
-      end
-
-      def update_product_params
-        params.permit(
-          product_attributes + [
-            yahoo_auction_crawl_setting: yahoo_auction_crawl_setting_attributes,
-            mercari_crawl_setting: mercari_crawl_setting_attributes,
-            janpara_crawl_setting: janpara_crawl_setting_attributes,
-            iosys_crawl_setting: iosys_crawl_setting_attributes,
-            pc_koubou_crawl_setting: pc_koubou_crawl_setting_attributes,
-            used_sofmap_crawl_setting: used_sofmap_crawl_setting_attributes
+            yahoo_auction_crawl_setting: yahoo_auction_crawl_setting_attributes + [
+              {
+                yahoo_auction_crawl_setting_exclude_keywords: yahoo_auction_crawl_setting_exclude_keyword_attributes,
+                yahoo_auction_crawl_setting_required_keywords: yahoo_auction_crawl_setting_required_keyword_attributes
+              }
+            ],
+            mercari_crawl_setting: mercari_crawl_setting_attributes + [
+              {
+                mercari_crawl_setting_exclude_keywords: mercari_crawl_setting_exclude_keyword_attributes,
+                mercari_crawl_setting_required_keywords: mercari_crawl_setting_required_keyword_attributes
+              }
+            ],
+            janpara_crawl_setting: janpara_crawl_setting_attributes + [
+              {
+                janpara_crawl_setting_exclude_keywords: janpara_crawl_setting_exclude_keyword_attributes,
+                janpara_crawl_setting_required_keywords: janpara_crawl_setting_required_keyword_attributes
+              }
+            ],
+            iosys_crawl_setting: iosys_crawl_setting_attributes + [
+              {
+                iosys_crawl_setting_exclude_keywords: iosys_crawl_setting_exclude_keyword_attributes,
+                iosys_crawl_setting_required_keywords: iosys_crawl_setting_required_keyword_attributes
+              }
+            ],
+            pc_koubou_crawl_setting: pc_koubou_crawl_setting_attributes + [
+              {
+                pc_koubou_crawl_setting_exclude_keywords: pc_koubou_crawl_setting_exclude_keyword_attributes,
+                pc_koubou_crawl_setting_required_keywords: pc_koubou_crawl_setting_required_keyword_attributes
+              }
+            ],
+            used_sofmap_crawl_setting: used_sofmap_crawl_setting_attributes + [
+              {
+                used_sofmap_crawl_setting_exclude_keywords: used_sofmap_crawl_setting_exclude_keyword_attributes,
+                used_sofmap_crawl_setting_required_keywords: used_sofmap_crawl_setting_required_keyword_attributes
+              }
+            ]
           ] + [:category_id]
         )
       end
@@ -73,24 +90,72 @@ module Api
         %i[keyword category_id min_price max_price enabled]
       end
 
+      def yahoo_auction_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def yahoo_auction_crawl_setting_required_keyword_attributes
+        %i[keyword]
+      end
+
       def mercari_crawl_setting_attributes
         %i[keyword category_id min_price max_price enabled]
+      end
+
+      def mercari_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def mercari_crawl_setting_required_keyword_attributes
+        %i[keyword]
       end
 
       def janpara_crawl_setting_attributes
         %i[keyword min_price max_price enabled]
       end
 
+      def janpara_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def janpara_crawl_setting_required_keyword_attributes
+        %i[keyword]
+      end
+
       def iosys_crawl_setting_attributes
         %i[keyword min_price max_price enabled]
+      end
+
+      def iosys_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def iosys_crawl_setting_required_keyword_attributes
+        %i[keyword]
       end
 
       def pc_koubou_crawl_setting_attributes
         %i[keyword min_price max_price enabled]
       end
 
+      def pc_koubou_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def pc_koubou_crawl_setting_required_keyword_attributes
+        %i[keyword]
+      end
+
       def used_sofmap_crawl_setting_attributes
         %i[keyword min_price max_price enabled]
+      end
+
+      def used_sofmap_crawl_setting_exclude_keyword_attributes
+        %i[keyword]
+      end
+
+      def used_sofmap_crawl_setting_required_keyword_attributes
+        %i[keyword]
       end
 
       def external_attributes
