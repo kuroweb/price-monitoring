9cd993ca04a967e972d8254370d39e4620fcc139
kuroweb <kuromitsu0104@gmail.com>
2025-01-30 01:06:57 +0900

    feat: 不要コードを撤去する

--- volumes/frontend/app/admin/products/[id]/page.tsx
+++ volumes/frontend/app/admin/products/[id]/page.tsx
@@ -1,13 +1,9 @@
 import Link from 'next/link'
 
 import Layout from '@/components/layouts/Layout'
-// import BulkEditExcludeKeywordModal from '@/features/admin/products/components/bulk-edit-exclude-keyword-modal/BulkEditExcludeKeywordModal'
-// import BulkEditRequiredKeywordModal from '@/features/admin/products/components/bulk-edit-required-keyword-modal/BulkEditRequiredKeywordModal'
 // import EditExcludeProductModal from '@/features/admin/products/components/edit-exclude-product-modal/EditExcludeProductModal'
 import UpdateProductModal from '@/features/admin/products/common/components/update-product-modal/UpdateProductModal'
 import { useUpdateProductModalQuery } from '@/features/admin/products/common/components/update-product-modal/hooks/useUpdateProductModalState'
-import { useBulkEditExcludeKeywordModalQuery } from '@/features/admin/products/components/bulk-edit-exclude-keyword-modal/hooks/useBulkEditExcludeKeywordModalState'
-import { useBulkEditRequiredKeywordModalQuery } from '@/features/admin/products/components/bulk-edit-required-keyword-modal/hooks/useBulkEditRequiredKeywordModalState'
 import { useEditExcludeProductModalQuery } from '@/features/admin/products/components/edit-exclude-product-modal/hooks/useEditExcludeProductModalState'
 import { getCategories, getProduct } from '@/server-actions/api'
 
@@ -45,35 +41,6 @@ const Page = async ({ params }: { params: { [key: string]: string | undefined } 
                   }}
                 >
                   除外商品ID
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className='card w-full bg-neutral'>
-            <div className='card-body'>
-              <h2 className='card-title pb-4'>一括設定</h2>
-              <div className='grid grid-cols-2 gap-4 md:grid-cols-4'>
-                <Link
-                  className='btn'
-                  href={{
-                    pathname: currentPathname,
-                    query: {
-                      [useBulkEditRequiredKeywordModalQuery]: 'true',
-                    },
-                  }}
-                >
-                  必須キーワード
-                </Link>
-                <Link
-                  className='btn'
-                  href={{
-                    pathname: currentPathname,
-                    query: {
-                      [useBulkEditExcludeKeywordModalQuery]: 'true',
-                    },
-                  }}
-                >
-                  除外キーワード
                 </Link>
               </div>
             </div>
