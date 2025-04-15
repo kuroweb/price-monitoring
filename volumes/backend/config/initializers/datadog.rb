31661a20485372bd8b91e664add30c46cfd9c119
kuroweb <kuromitsu0104@gmail.com>
2025-04-16 01:26:41 +0900

    feat: update

--- volumes/backend/config/initializers/datadog.rb
+++ volumes/backend/config/initializers/datadog.rb
@@ -9,9 +9,21 @@ if Rails.env.production?
   Datadog.configure do |c|
     c.env = 'production'
     c.service = 'price-monitoring'
+
+    c.use :rails
+    c.use :rack
+    c.use :active_record
+    c.use :sidekiq
+    c.use :redis
+    c.use :http
+
     c.tracing.transport_options = proc do |t|
-      # TODO: hostnameをENVに切り出す
-      t.adapter :net_http, timeout: 1, hostname: 'datadog-agent.datadog.svc.cluster.local', port: 8126
+      t.adapter
+        :net_http,
+        timeout: 1,
+        # TODO: hostnameをENVに切り出す
+        hostname: 'datadog-agent.datadog.svc.cluster.local',
+        port: 8126
     end
   end
 end
