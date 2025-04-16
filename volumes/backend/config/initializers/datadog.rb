c2408cf24ae7485e994a531da2d9a14454c44ed3
kuroweb <kuromitsu0104@gmail.com>
2025-04-16 22:58:30 +0900

    feat: update

--- volumes/backend/config/initializers/datadog.rb
+++ volumes/backend/config/initializers/datadog.rb
@@ -1,21 +1,25 @@
 if Rails.env.production?
-  require 'opentracing'
-  require 'datadog/opentracer'
-  require 'ddtrace'
+  require "opentracing"
+  require "datadog/opentracer"
+  require "ddtrace"
 
   # Activate the Datadog tracer for OpenTracing
   OpenTracing.global_tracer = Datadog::OpenTracer::Tracer.new
 
   Datadog.configure do |c|
-    c.env = 'production'
-    c.service = 'price-monitoring'
+    c.env = "production"
+    c.service = "price-monitoring"
 
     c.tracing.transport_options = proc do |t|
       t.adapter :net_http,
-        timeout: 1,
-        # TODO: hostnameをENVに切り出す
-        hostname: 'datadog-agent.datadog.svc.cluster.local',
-        port: 8126
+                timeout: 1,
+                # TODO: hostnameをENVに切り出す
+                hostname: "datadog-agent.datadog.svc.cluster.local",
+                port: 8126
     end
+
+    c.tracing.instrument :rails
+    c.tracing.instrument :rack
+    c.tracing.instrument :sidekiq
   end
 end
