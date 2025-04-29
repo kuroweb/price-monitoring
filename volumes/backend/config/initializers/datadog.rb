d1ecb2cd78ffb5bf2bae42bc4169376966ded1ec
kuroweb <kuromitsu0104@gmail.com>
2025-04-29 09:12:39 +0900

    feat: datadogトレースを調整

--- volumes/backend/config/initializers/datadog.rb
+++ volumes/backend/config/initializers/datadog.rb
@@ -11,7 +11,6 @@ if Rails.env.production?
 
     c.tracing.instrument :rails, service_name: app
     c.tracing.instrument :sidekiq, service_name: "#{app}-sidekiq",
-                                   client_service_name: "#{app}-sidekiq-client",
                                    tag_args: true
     c.tracing.instrument :active_record, service_name: "#{app}-mysql"
     c.tracing.instrument :http, service_name: "#{app}-http"
