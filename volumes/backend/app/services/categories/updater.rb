3e6e8319a063f96803381e8ce143d77f188fdb41
kuroweb <kuromitsu0104@gmail.com>
2026-03-18 07:43:24 +0900

    Fix root category disappearing after update

--- volumes/backend/app/services/categories/updater.rb
+++ volumes/backend/app/services/categories/updater.rb
@@ -6,16 +6,27 @@ module Categories
 
     def initialize(category:, params:)
       @category = category
+      @has_parent_id_param = params.key?(:parent_id)
       @parent_id = params[:parent_id]
       @name = params[:name]
     end
 
     def call
-      category.update!(name:, parent_id:)
+      category.update!(name:, parent_id: resolved_parent_id)
     end
 
     private
 
-    attr_reader :category, :parent_id, :name
+    attr_reader :category, :parent_id, :name, :has_parent_id_param
+
+    def resolved_parent_id
+      return category.parent_id unless has_parent_id_param
+      return nil if parent_id.blank?
+
+      id = Integer(parent_id, exception: false)
+      return nil if id.blank? || id.zero?
+
+      Category.find(id).id
+    end
   end
 end
